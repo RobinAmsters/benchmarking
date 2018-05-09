4e5cef2347a7a14a08562e2ce940586a4475f27e
Robin <robin.amsters@kuleuven.be>
2018-05-09 16:57:27 +0200

    start marker detection example

---
+++ examples/marker_detection.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 16:19:59 2018
+
+@author: Robin Amsters
+@email: robin.amsters@kuleuven.be
+"""
+
+from file_select_gui import get_file_path
+from webcam import get_webcam_reference
+
+videoFilePath
+
+get_webcam_reference(videoFilePath, cParamsFilePath, dictionary, markerSize, pose_timeout=0, show_video=True)
